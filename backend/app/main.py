cd9213e7f108ca7d2a7491431103caf7845c4fc3
mazyar <callmemazyar@gmail.com>
2026-08-06 03:24:01 +0330

    edit main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from typing import Dict
-from face_detector import FaceDetector
-from analyser import Analyser
-from utils import image_from_upload
+from .face_detector import FaceDetector
+from .analyser import Analyser
+from .utils import image_from_upload
 
 
 app = FastAPI(title="AreWeLookalike API")
